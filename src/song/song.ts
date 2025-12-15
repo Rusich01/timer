69386dc53820b5e784a1210539a2342ac8a6e9a6
Ruslan Hrytsai <103769765+Rusich01@users.noreply.github.com>
2025-12-16 00:47:42 +0100

    add sound

---
+++ src/song/song.ts
@@ -0,0 +1,5 @@
+import stopClickSound from "./stop-click.mp3";
+import startClickSound from "./start-click.mp3";
+
+export const soundStop = new Audio(stopClickSound);
+export const soundStart = new Audio(startClickSound);
